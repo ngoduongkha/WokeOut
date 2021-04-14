bf3e14d7feffa8a08797de0e75ba21ea35e7a6c0
ltl13 <66907671+ltl13@users.noreply.github.com>
2021-04-14 16:57:29 +0700

    my data update

--- lib/data/exercise_data.dart
+++ lib/data/exercise_data.dart
@@ -2,157 +2,36 @@ import 'package:woke_out/model/exercise.dart';
 
 final exercises1 = [
   Exercise(
-    name: 'Burpees',
+    name: 'Pull up',
     duration: Duration(seconds: 30),
-    noOfReps: 12,
+    noOfReps: 5,
   ),
   Exercise(
-    name: 'Push Up',
-    duration: Duration(seconds: 40),
-    noOfReps: 10,
+    name: 'Chin up',
+    duration: Duration(seconds: 30),
+    noOfReps: 5,
   ),
   Exercise(
-    name: 'Plank',
+    name: 'Row pull',
     duration: Duration(seconds: 20),
-    noOfReps: 15,
-  ),
-  Exercise(
-    name: 'Pull Up',
-    duration: Duration(seconds: 50),
     noOfReps: 8,
-  ),
-  Exercise(
-    name: 'Crunch',
-    duration: Duration(seconds: 10),
-    noOfReps: 12,
-  ),
-  Exercise(
-    name: 'Lunges',
-    duration: Duration(seconds: 20),
-    noOfReps: 15,
-  ),
-  Exercise(
-    name: 'Squats',
-    duration: Duration(seconds: 30),
-    noOfReps: 20,
-  ),
-  Exercise(
-    name: 'Burpees',
-    duration: Duration(seconds: 30),
-    noOfReps: 10,
   ),
 ];
 
 final exercises2 = [
   Exercise(
-    name: 'Push Up',
-    duration: Duration(seconds: 40),
-    noOfReps: 6,
-  ),
-  Exercise(
-    name: 'Side Jump',
-    duration: Duration(seconds: 30),
-    noOfReps: 6,
-  ),
-  Exercise(
-    name: 'Plank',
-    duration: Duration(seconds: 20),
-    noOfReps: 8,
-  ),
-  Exercise(
     name: 'Crunch',
-    duration: Duration(seconds: 25),
-    noOfReps: 10,
-  ),
-  Exercise(
-    name: 'Lunges',
-    duration: Duration(seconds: 35),
-    noOfReps: 12,
-  ),
-  Exercise(
-    name: 'Squats',
-    duration: Duration(seconds: 10),
-    noOfReps: 5,
-  ),
-  Exercise(
-    name: 'Burpees',
-    duration: Duration(seconds: 50),
-    noOfReps: 15,
-  ),
-];
-
-final exercises3 = [
-  Exercise(
-    name: 'Burpees',
-    duration: Duration(seconds: 60),
+    duration: Duration(seconds: 40),
     noOfReps: 20,
   ),
   Exercise(
-    name: 'Push Up',
-    duration: Duration(seconds: 10),
-    noOfReps: 5,
+    name: 'V up',
+    duration: Duration(seconds: 40),
+    noOfReps: 20,
   ),
   Exercise(
-    name: 'Side Jump',
+    name: 'L sit',
     duration: Duration(seconds: 20),
-    noOfReps: 8,
-  ),
-  Exercise(
-    name: 'Plank',
-    duration: Duration(seconds: 25),
     noOfReps: 1,
-  ),
-  Exercise(
-    name: 'Crunch',
-    duration: Duration(seconds: 25),
-    noOfReps: 12,
-  ),
-  Exercise(
-    name: 'Lunges',
-    duration: Duration(seconds: 40),
-    noOfReps: 16,
-  ),
-  Exercise(
-    name: 'Squats',
-    duration: Duration(seconds: 20),
-    noOfReps: 14,
-  ),
-];
-
-final exercises4 = [
-  Exercise(
-    name: 'Lunges',
-    duration: Duration(seconds: 40),
-    noOfReps: 6,
-  ),
-  Exercise(
-    name: 'Burpees',
-    duration: Duration(seconds: 10),
-    noOfReps: 10,
-  ),
-  Exercise(
-    name: 'Push Up',
-    duration: Duration(seconds: 20),
-    noOfReps: 8,
-  ),
-  Exercise(
-    name: 'Side Jump',
-    duration: Duration(seconds: 25),
-    noOfReps: 14,
-  ),
-  Exercise(
-    name: 'Plank',
-    duration: Duration(seconds: 15),
-    noOfReps: 12,
-  ),
-  Exercise(
-    name: 'Crunch',
-    duration: Duration(seconds: 120),
-    noOfReps: 20,
-  ),
-  Exercise(
-    name: 'Squats',
-    duration: Duration(seconds: 30),
-    noOfReps: 15,
   ),
 ];

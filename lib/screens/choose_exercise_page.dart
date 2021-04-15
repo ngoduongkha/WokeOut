3c330f520750b882011c833bcdd105d74088a4a0
ltl13 <66907671+ltl13@users.noreply.github.com>
2021-04-15 11:09:49 +0700

    a

--- lib/screens/choose_exercise_page.dart
+++ lib/screens/choose_exercise_page.dart
@@ -2,5 +2,17 @@ import 'package:flutter/material.dart';
 
 class ChooseExercisePage extends StatelessWidget {
   @override
-  Widget build(BuildContext context) => Scaffold();
+  Widget build(BuildContext context) => Scaffold(
+        body: Container(
+          height: double.infinity,
+          width: double.infinity,
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage('assets/images/choose_exercise_page.jpg'),
+              fit: BoxFit.cover,
+            ),
+          ),
+          child: CustomScrollView(),
+        ),
+      );
 }

e103413a1623de37fc68f2b1e4eefced2f767b52
ltl13 <66907671+ltl13@users.noreply.github.com>
2021-04-15 15:05:16 +0700

    l

--- lib/screens/choose_exercise_page.dart
+++ lib/screens/choose_exercise_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class ChooseExercisePage extends StatelessWidget {
   @override
@@ -13,7 +14,45 @@ class ChooseExercisePage extends StatelessWidget {
             ),
           ),
           child: CustomScrollView(
-            slivers: [],
+            slivers: [
+              buildAppBar(context),
+            ],
+          ),
+        ),
+      );
+  Widget buildAppBar(BuildContext context) => SliverAppBar(
+        expandedHeight: MediaQuery.of(context).size.height * 0.5,
+        backgroundColor: Colors.transparent,
+        title: Row(
+          children: [
+            Text(
+              'WELCOME,  ',
+              style: GoogleFonts.bebasNeue(
+                fontSize: 43,
+                color: Colors.white,
+                letterSpacing: 1.8,
+              ),
+            ),
+            Text(
+              'LUAN',
+              style: GoogleFonts.bebasNeue(
+                fontSize: 43,
+                color: Color(0xFF40D876),
+                letterSpacing: 1.8,
+              ),
+            ),
+          ],
+        ),
+        flexibleSpace: Container(
+          width: 100,
+          height: 100,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(100),
+            border: Border.all(width: 3, color: Color(0xFF40D876)),
+            image: DecorationImage(
+              image: AssetImage('assets/images/avartar_demo.jpg'),
+              fit: BoxFit.cover,
+            ),
           ),
         ),
       );

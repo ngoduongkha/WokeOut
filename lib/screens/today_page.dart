eb902ccc8cd07a36b80839a7e0b188cbc76d146e
ltl13 <66907671+ltl13@users.noreply.github.com>
2021-04-17 14:01:23 +0700

    control stream added

--- lib/screens/today_page.dart
+++ lib/screens/today_page.dart
@@ -1,14 +1,33 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:woke_out/screens/today_exercises_page.dart';
+
+final StreamController<bool> streamController = StreamController<bool>();
 
 class TodayPage extends StatefulWidget {
+  final Stream<bool> stream = streamController.stream;
   @override
   _TodayPageState createState() => _TodayPageState();
 }
 
 class _TodayPageState extends State<TodayPage> {
   var showChooseExercise = true;
+
+  @override
+  void initState() {
+    super.initState();
+    widget.stream.listen((event) {
+      mySetState(event);
+    });
+  }
+
+  void mySetState(bool status) {
+    setState(() {
+      showChooseExercise = status;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return showChooseExercise
@@ -200,9 +219,7 @@ class _TodayPageState extends State<TodayPage> {
             child: Column(
               children: [
                 GestureDetector(
-                  onTap: () => setState(() {
-                    showChooseExercise = false;
-                  }),
+                  onTap: () => streamController.add(false),
                   child: Container(
                     height: 200,
                     width: 280,

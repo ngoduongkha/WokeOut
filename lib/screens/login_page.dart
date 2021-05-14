1d0df3145de4cc2f9dde239834e5cd20b0def65b
ltl13 <66907671+ltl13@users.noreply.github.com>
2021-05-14 15:47:23 +0700

    login

--- lib/screens/login_page.dart
+++ lib/screens/login_page.dart
@@ -28,110 +28,116 @@ class _Body extends StatelessWidget {
     Size size = MediaQuery.of(context).size;
     final auth = Provider.of<AuthService>(context, listen: false);
 
-    _background() {
-      return Background(
-        child: SingleChildScrollView(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Text(
-                "LOGIN",
-                style: TextStyle(fontWeight: FontWeight.bold),
-              ),
-              SizedBox(height: size.height * 0.03),
-              SvgPicture.asset(
-                "assets/icons/login.svg",
-                height: size.height * 0.35,
-              ),
-              SizedBox(height: size.height * 0.03),
-              RoundedInputField(
-                hintText: "Your Email",
-                controller: emailController,
-                onChanged: (value) {},
-              ),
-              RoundedPasswordField(
-                controller: passwordController,
-                onChanged: (value) {},
-              ),
-              RoundedButton(
-                text: "LOGIN",
-                press: () async {
-                  var user = await auth.signInWithEmailAndPassword(
-                      emailController.text, passwordController.text);
-                  if (user != null) {
-                    Navigator.pushNamedAndRemoveUntil(
-                        context, 'home', ModalRoute.withName('landing'));
-                  } else {
-                    showDialog(
-                      context: context,
-                      builder: (BuildContext context) {
-                        return CustomDialogBox(
-                          dialogType: DialogType.error,
-                          title: "Đăng nhập thất bại",
-                          descriptions: auth.errorMessage,
-                          text: "OK",
-                        );
-                      },
-                    );
-                  }
-                },
-              ),
-              SizedBox(height: size.height * 0.03),
-              AlreadyHaveAnAccountCheck(
-                press: () {
-                  Navigator.popAndPushNamed(context, 'signup');
-                },
-              ),
-            ],
-          ),
-        ),
-      );
-    }
+    // _background() {
+    //   return Background(
+    //     child: SingleChildScrollView(
+    //       child: Column(
+    //         mainAxisAlignment: MainAxisAlignment.center,
+    //         children: <Widget>[
+    //           Text(
+    //             "LOGIN",
+    //             style: TextStyle(fontWeight: FontWeight.bold),
+    //           ),
+    //           SizedBox(height: size.height * 0.03),
+    //           SizedBox(height: size.height * 0.03),
+    //           RoundedInputField(
+    //             hintText: "Your Email",
+    //             controller: emailController,
+    //             onChanged: (value) {},
+    //           ),
+    //           RoundedPasswordField(
+    //             controller: passwordController,
+    //             onChanged: (value) {},
+    //           ),
+    //           RoundedButton(
+    //             text: "LOGIN",
+    //             press: () async {
+    //               var user = await auth.signInWithEmailAndPassword(
+    //                   emailController.text, passwordController.text);
+    //               if (user != null) {
+    //                 Navigator.pushNamedAndRemoveUntil(
+    //                     context, 'home', ModalRoute.withName('landing'));
+    //               } else {
+    //                 showDialog(
+    //                   context: context,
+    //                   builder: (BuildContext context) {
+    //                     return CustomDialogBox(
+    //                       dialogType: DialogType.error,
+    //                       title: "Đăng nhập thất bại",
+    //                       descriptions: auth.errorMessage,
+    //                       text: "OK",
+    //                     );
+    //                   },
+    //                 );
+    //               }
+    //             },
+    //           ),
+    //           SizedBox(height: size.height * 0.03),
+    //           AlreadyHaveAnAccountCheck(
+    //             press: () {
+    //               Navigator.popAndPushNamed(context, 'signup');
+    //             },
+    //           ),
+    //         ],
+    //       ),
+    //     ),
+    //   );
+    // }
 
     return Scaffold(
-      body: Stack(
-        children: <Widget>[
-          _background(),
-        ],
-      ),
-    );
-  }
-}
-
-class Background extends StatelessWidget {
-  final Widget child;
-
-  Background({
-    @required this.child,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    Size size = MediaQuery.of(context).size;
-    return Container(
-      height: size.height,
-      width: double.infinity,
-      child: Stack(
-        alignment: Alignment.center,
-        children: <Widget>[
-          Positioned(
-            top: 0,
-            left: 0,
-            child: Image.asset(
-              "assets/images/main_top.png",
-              width: size.width * 0.35,
-            ),
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/images/signin_background.jpg'),
+            fit: BoxFit.cover,
           ),
-          Positioned(
-            bottom: 0,
-            right: 0,
-            child: Image.asset(
-              "assets/images/login_bottom.png",
-              width: size.width * 0.4,
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: <Widget>[
+            SizedBox(height: size.height * 0.03),
+            SizedBox(height: size.height * 0.03),
+            RoundedInputField(
+              hintText: "Your Email",
+              controller: emailController,
+              onChanged: (value) {},
             ),
-          ),
-          child,
-        ],
+            RoundedPasswordField(
+              controller: passwordController,
+              onChanged: (value) {},
+            ),
+            RoundedButton(
+              text: "LOGIN",
+              press: () async {
+                var user = await auth.signInWithEmailAndPassword(
+                    emailController.text, passwordController.text);
+                if (user != null) {
+                  Navigator.pushNamedAndRemoveUntil(
+                      context, 'home', ModalRoute.withName('landing'));
+                } else {
+                  showDialog(
+                    context: context,
+                    builder: (BuildContext context) {
+                      return CustomDialogBox(
+                        dialogType: DialogType.error,
+                        title: "Đăng nhập thất bại",
+                        descriptions: auth.errorMessage,
+                        text: "OK",
+                      );
+                    },
+                  );
+                }
+              },
+            ),
+            SizedBox(height: size.height * 0.03),
+            AlreadyHaveAnAccountCheck(
+              press: () {
+                Navigator.popAndPushNamed(context, 'signup');
+              },
+            ),
+            SizedBox(height: size.height * 0.1),
+          ],
+        ),
       ),
     );
   }
